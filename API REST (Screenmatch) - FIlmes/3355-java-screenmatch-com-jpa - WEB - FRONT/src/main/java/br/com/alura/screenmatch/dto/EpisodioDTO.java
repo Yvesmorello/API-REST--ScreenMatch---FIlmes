e0c187c9e44bb3c42217a1f155d636dbd22b7fab
Yvesmorello <124888523+Yvesmorello@users.noreply.github.com>
2024-08-10 15:07:56 -0300

    Delete API REST (Screenmatch) - FIlmes directory

--- API REST (Screenmatch) - FIlmes/3355-java-screenmatch-com-jpa - WEB - FRONT/src/main/java/br/com/alura/screenmatch/dto/EpisodioDTO.java
+++
@@ -1,8 +0,0 @@
-package br.com.alura.screenmatch.dto;
-
-import com.fasterxml.jackson.annotation.JsonAlias;
-
-public record EpisodioDTO( Integer temporada,
-                           Integer numeroEpisodio,
-                           String titulo){
-}
